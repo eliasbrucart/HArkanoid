aa50f50d686e3fff13a69859758a1288f64815b4
Elias <eliasbrucart@hardgames.com.ar>
2021-06-01 02:48:16 -0300

    Now bricks are destroyed on collision with ball

---
+++ Assets/Scripts/Bricks/DestroyBrick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBrick : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ball"))
+            Destroy(gameObject);
+    }
+}
