c8986019fb133ca0b8071be3179fd9b4a7aa2a79
Elias <eliasbrucart@hardgames.com.ar>
2021-06-01 02:31:14 -0300

    Basic function of the player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,22 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private int lives;
+    [SerializeField] private Vector3 offsetWithBall;
+
+    private int livesToSubtract = 1;
 
     private Rigidbody rb;
+
+    private Ball ball;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ball = FindObjectOfType<Ball>();
+        OffsetWithBall();
+        Ball.PlayerSubtractLives += SubtractLives;
+    }
+
+    private void Update()
+    {
+        InputPlayer();
+        OffsetWithBall();
     }
 
     void FixedUpdate()
     {
+        MovePlayer();
+    }
+
+    private void OffsetWithBall()
+    {
+        if (!ball.isMoving)
+            ball.transform.position = transform.position - offsetWithBall;
+    }
+
+    private void MovePlayer()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
         rb.velocity = Vector3.right * horizontalInput * speed;
+    }
+
+    void InputPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ball.isMoving = true;
+        }
+    }
+
+    private void SubtractLives()
+    {
+        lives -= livesToSubtract;
+        if (lives <= 0)
+            lives = 0;
+        Debug.Log("resto una vida! vidas: " + lives);
+    }
+
+    private void OnDisable()
+    {
+        Ball.PlayerSubtractLives -= SubtractLives;
     }
 }
