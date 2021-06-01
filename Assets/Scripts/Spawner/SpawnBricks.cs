ac054e4649aef4063a0be8d0de168b4aae48d075
Elias <eliasbrucart@hardgames.com.ar>
2021-05-31 21:10:39 -0300

    Added scenario in 3D

---
+++ Assets/Scripts/Spawner/SpawnBricks.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SpawnBricks : MonoBehaviour
+{
+    [SerializeField] private GameObject brick;
+    [SerializeField] private int numberOfBlocks;
+    [SerializeField] private int initialPosX;
+    [SerializeField] private int initialPosY;
+    [SerializeField] private int separationPosX;
+    void Start()
+    {
+        for(int i = 0; i < numberOfBlocks; i++)
+        {
+            GameObject currentBrick = Instantiate(brick, transform);
+            currentBrick.transform.position = new Vector2(initialPosX + (separationPosX * i), initialPosY);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
