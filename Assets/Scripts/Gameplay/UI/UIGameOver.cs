76f5f228310f527043c5cc2773da9e1761e0694a
Elias <eliasbrucart@hardgames.com.ar>
2021-06-01 23:08:43 -0300

    Added game over scene

---
+++ Assets/Scripts/Gameplay/UI/UIGameOver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour
+{
+    public Text scoreEarned;
+    public Text highScore;
+
+    void Update()
+    {
+        scoreEarned.text = "Score: " + GameManager.instanceGameManager.score;
+        highScore.text = "High Score: " + GameManager.instanceGameManager.highScore;
+    }
+}
